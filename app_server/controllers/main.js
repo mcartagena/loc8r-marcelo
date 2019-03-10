d7301fd9bf4766cbf5f0dc4dd9f4d0dde7114040
mcartagena <marcelocartagenaorellana@gmail.com>
2019-03-10 14:04:53 -0300

    probando el deploy automatico

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,4 +1,4 @@
 /* GET home page*/
 module.exports.index = function(req, res){
-    res.render('index',{title:'My Express, Marchelito lindo te amo!'})
+    res.render('index',{title:'My Express, Marchelito lindo te amo! Y ahora con deploy automatico a Heroku.'})
 };
