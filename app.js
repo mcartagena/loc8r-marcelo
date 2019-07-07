49bdc1a73b636f7a9810a52e3351ab384220eacd
mcartagena <marcelocartagenaorellana@gmail.com>
2019-07-07 19:42:45 -0400

    agrego directivas header, navigation y footer

--- app.js
+++ app.js
@@ -23,7 +23,10 @@ var appClientFiles = [
   'app_client/common/services/geolocation.service.js',                 
   'app_client/common/services/loc8rData.service.js',                   
   'app_client/common/filters/formatDistance.filter.js',                
-  'app_client/common/directives/ratingStars/ratingStars.directive.js'  
+  'app_client/common/directives/ratingStars/ratingStars.directive.js',
+  'app_client/common/directives/navigation/navigation.directive.js',
+  'app_client/common/directives/footerGeneric/footerGeneric.directive.js',
+  'app_client/common/directives/pageHeader/pageHeader.directive.js'  
 ];                                                                     
 var uglified = uglifyJs.minify(appClientFiles, { compress : false });   
 fs.writeFile('public/angular/loc8r.min.js', uglified.code, function (err){
@@ -41,9 +44,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app_client')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/api', routesApi);
 app.use('/users', usersRouter);
+
+app.use(function(req, res) {
+  res.sendFile(path.join(__dirname, 'app_client', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
