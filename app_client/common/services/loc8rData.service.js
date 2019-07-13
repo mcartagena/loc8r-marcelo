b2b8899bde42ac3e0cf2e3651e0aaa759dd19171
mcartagena <marcelocartagenaorellana@gmail.com>
2019-07-13 15:13:48 -0400

    agrego modal para add review

--- app_client/common/services/loc8rData.service.js
+++ app_client/common/services/loc8rData.service.js
@@ -12,9 +12,14 @@ function loc8rData ($http) {
   var locationById = function (locationid) {        
     return $http.get('/api/locations/' + locationid);    
   };
+  var addReviewById = function (locationid, data) {
+    return $http.post('/api/locations/' + locationid + '/reviews', data);
+  };
+
   return {
     locationByCoords : locationByCoords,
-    locationById : locationById
+    locationById : locationById,
+    addReviewById : addReviewById
   };
 }
 
--- app_client/locationDetail/locationDetail.controller.js
+++ app_client/locationDetail/locationDetail.controller.js
@@ -3,8 +3,8 @@
         .module('loc8rApp')
         .controller('locationDetailCtrl', locationDetailCtrl);
 
-    locationDetailCtrl.$inject = ['$routeParams', 'loc8rData'];
-    function locationDetailCtrl($routeParams, loc8rData) {
+    locationDetailCtrl.$inject = ['$routeParams', '$modal', 'loc8rData'];
+    function locationDetailCtrl($routeParams, $modal, loc8rData) {
         var vm = this;
         vm.locationid = $routeParams.locationid;
 
@@ -18,5 +18,27 @@
             .error(function (e) {
                 console.log(e);
             });
+
+        vm.popupReviewForm = function () {
+            var modalInstance = $modal.open({
+                templateUrl: '/reviewModal/reviewModal.view.html',
+                controller: 'reviewModalCtrl as vm',
+                resolve: {
+                    locationData: function () {
+                        return {
+                            locationid: vm.locationid,
+                            locationName: vm.data.location.name
+                        };
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (data) {
+                vm.data.location.reviews.push(data);
+            });
+
+        };
+
     }
+
 })();
