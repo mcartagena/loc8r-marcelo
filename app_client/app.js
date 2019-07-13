4a61d883b4aea6e95bec139662b6a809ef131e8a
mcartagena <marcelocartagenaorellana@gmail.com>
2019-07-13 09:27:11 -0400

    Se agrega pagina de detalle de location

--- app_client/app.js
+++ app_client/app.js
@@ -14,6 +14,11 @@ function config($routeProvider, $locationProvider) {
       controller: 'aboutCtrl',    
       controllerAs: 'vm'              
     })
+    .when('/location/:locationid', {
+      templateUrl: '/locationDetail/locationDetail.view.html',
+      controller: 'locationDetailCtrl',
+      controllerAs: 'vm'
+    })
     .otherwise({ redirectTo: '/' });
 
     $locationProvider.html5Mode(true);
